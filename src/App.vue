f9367857706e5d5e3386a418dca8d17eff77737a
donbatex <batex2k82yahoo.com>
2026-06-10 04:56:24 +0100

    Add data fetching logic to App.vue for prompt generations

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+const prompts = ref([])
+
+onMounted(() => {
+  fetch('http://localhost:8000/api/v1/prompt-generations', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer 8|t6UElHGIyrlmBIIvrJGc3Wv9Gd61Z6V0WfqLfqtC025a180f`
+    }
+  })
+  
+  .then(response => response.json())
+  .then($result => {
+    debugger;
+    prompts.value = result.data
+  })
+})
 </script>
 
 <template>
